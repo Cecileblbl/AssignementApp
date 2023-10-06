b48cc0b203986806d1099c25146858a886354fb1
Cecile <cecile.barouk@gmail.com>
2023-10-06 15:01:23 +0200

    slide 43

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
@@ -8,4 +7,15 @@ import { Component, OnInit } from '@angular/core';
 export class AssignmentsComponent implements OnInit {
   ngOnInit(): void {}
   titre = 'Mon application sur les Assignments !';
+  assignments = [
+    {nom:"TP1 sur Webdev", 
+    dateDeRendu:new Date('2021-03-15'),
+    rendu : true},
+    {nom:"TP2 sur angular",
+    dateDeRendu:new Date('2021-03-22'),
+    rendu : false},
+    {nom:"TP3 Management de projet",
+    dateDeRendu:new Date('2021-03-29'),
+    rendu : false}
+  ];
 }
