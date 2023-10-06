67a234e7158a96eeccb28cd210993d19275a392f
Cecile <cecile.barouk@gmail.com>
2023-10-06 14:17:48 +0200

    Slide 38

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-assignments',
   templateUrl: './assignments.component.html',
   styleUrls: ['./assignments.component.css']
 })
-export class AssignmentsComponent {
-
+export class AssignmentsComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  titre = "Mon application sur les Assignments !"
 }
